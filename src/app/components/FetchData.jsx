fa9bb31e78cba215e2a82560e10e772d8721b817
William Lowrimore <wlowrimore@gmail.com>
2023-11-18 01:53:09 -0600

    completed conditional rendering in dictionary

--- src/app/components/FetchData.jsx
+++ src/app/components/FetchData.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import AudioPlayer from './AudioPlayer';
 import Four0Four from './ui/four0four';
+import PagePlaceholder from './ui/PagePlaceholder';
 
 const API_KEY = process.env.NEXT_PUBLIC_DICT_API_KEY
 const BASE_URL = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
@@ -19,6 +20,8 @@ const WordMeaning = () => {
     try {
       const res = await fetch(`${BASE_URL}/${query}?key=${API_KEY}`)
       const data = await res.json();
+
+      console.log('API Response:', data);
 
       if (Array.isArray(data) && data.length > 0) {
         setEntry(data[0]);
@@ -48,11 +51,14 @@ const WordMeaning = () => {
     setQueryEntryMsg(!query ? 'Please enter your search term.' : '');
   }, [query]);
 
-  console.log(entry);
+  // console.log(entry);
+
+  const hasEntry = entry?.meta?.id;
 
   const artCaption = entry?.art?.capt?.replace(/{it}/g, '').replace(/{\/it}/g, '')
-
   const entryDate = entry?.date?.replace(/{ds\|\|[^}]+\|}/g, '');
+
+  const isQueryNotFound = queryErrMsg || !queryEntryMsg && !query;
 
   return (
     <div className='w-full'>
@@ -71,7 +77,6 @@ const WordMeaning = () => {
           {queryEntryMsg && !queryErrMsg && (
             <div>{queryEntryMsg}</div>
           )}
-
         </div>
         <button onClick={handleSearch} className='h-[2rem] flex px-2 gap-1 items-center border border-white rounded-lg text-white bg-sky-600 font-semibold hover:bg-white hover:text-sky-600 hover:scale-95 transform duration-300 active:outline-none'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -81,66 +86,70 @@ const WordMeaning = () => {
         </button>
       </section>
       <section className='w-full flex mx-auto'>
-        {entry?.meta?.id ? (
-          <div className='flex flex-col items-center w-full'>
-            {/* entry word and etymology */}
-            <div className='flex flex-col items-start mb-6 w-full mx-auto'>
-              <h1 className='text-4xl text-orange-300'>
-                {entry.meta.stems[0]}
-                <span className='ml-2 text-lg text-blue-100 tracking-wider'>({entry.hwi.hw})</span>
-              </h1>
-              <div className='flex w-full gap-1 mb-4'>
-                {entry?.uros?.map((uro, urosIndex) => (
-                  <p key={urosIndex} className='text-sm'><em>{uro.ure}</em>&nbsp;&nbsp;|</p>
-                ))}
-              </div>
-              <div>
-                {entryDate && (
-                  <p className='text-teal-200 text-sm mb-4'>First Known Use:&nbsp;{entryDate}</p>
-                )}
-              </div>
-              {entry?.et && (
-                <div>
-                  <p className='text-blue-200 font-light italic tracking-wide'>Etymology - <span className='text-rose-200'>{entry.et}</span></p>
-                </div>
-              )}
-              <AudioPlayer entry={entry} />
-              <div className='bg-neutral-300/20 h-[1px] w-full mt-6' />
-            </div>
-            {/* image and definition(s) */}
-            <div className='grid grid-cols-2 w-full mb-3'>
-              {entry?.art && artCaption ? (
-                <div className='w-full flex gap-6'>
-                  <div className='w-full flex justify-center'>
-                    <Image src={`https://www.merriam-webster.com/assets/mw/static/art/dict/${entry.art.artid}.gif`} alt='' width={400} height={300} className='rounded-lg my-2' />
-                  </div>
-                  <div className='flex justify-center'>
-                    <p className='w-[400px] text-justify'>{artCaption}</p>
-                  </div>
-                </div>
-              ) : (
-                <div className='flex items-center justify-center w-[400px] h-[300px] bg-neutral-600 border border-neutral-300 rounded-lg text-3xl'>Image not available</div>
-              )}
-            </div>
-            <div className='w-full'>
-              {entry.shortdef.map((definition, defIndex) => (
-                <div key={defIndex} className='flex gap-2 text-justify'>
-                  <p className='text-orange-300'>{defIndex + 1}.</p>
-                  <p className='text-blue-50 text-justify'>{definition}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div className='flex flex-col w-full items-center my-24'>
+        {isQueryNotFound ? (
+          <div className='flex flex-col w-full items-center my-12'>
             <Four0Four
               extractedQuery={extractedQuery}
             />
           </div>
+        ) : (
+          entry?.meta?.id ? (
+            <div className='flex flex-col items-center w-full'>
+              {/* entry word and etymology */}
+              <div className='flex flex-col items-start mb-6 w-full mx-auto'>
+                <h1 className='text-4xl text-orange-300'>
+                  {entry.meta.stems[0]}
+                  <span className='ml-2 text-lg text-blue-100 tracking-wider'>({entry.hwi.hw})</span>
+                </h1>
+                <div className='flex w-full gap-1 mb-4'>
+                  {entry?.uros?.map((uro, urosIndex) => (
+                    <p key={urosIndex} className='text-sm'><em>{uro.ure}</em>&nbsp;&nbsp;|</p>
+                  ))}
+                </div>
+                <div>
+                  {entryDate && (
+                    <p className='text-teal-200 text-sm mb-4'>First Known Use:&nbsp;{entryDate}</p>
+                  )}
+                </div>
+                {entry?.et && (
+                  <div>
+                    <p className='text-blue-200 font-light italic tracking-wide'>Etymology - <span className='text-rose-200'>{entry.et}</span></p>
+                  </div>
+                )}
+                <AudioPlayer entry={entry} />
+                <div className='bg-neutral-300/20 h-[1px] w-full mt-6' />
+              </div>
+              {/* image and definition(s) */}
+              <div className='grid grid-cols-2 w-full mb-3'>
+                {entry?.art && artCaption ? (
+                  <div className='w-full flex gap-6'>
+                    <div className='w-full flex justify-center'>
+                      <Image src={`https://www.merriam-webster.com/assets/mw/static/art/dict/${entry.art.artid}.gif`} alt='' width={400} height={300} className='rounded-lg my-2' />
+                    </div>
+                    <div className='flex justify-center'>
+                      <p className='w-[400px] text-justify'>{artCaption}</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div className='flex items-center justify-center w-[400px] h-[300px] bg-neutral-600 border border-neutral-300 rounded-lg text-3xl'>Image not available</div>
+                )}
+              </div>
+              <div className='w-full'>
+                {entry.shortdef.map((definition, defIndex) => (
+                  <div key={defIndex} className='flex gap-2 text-justify'>
+                    <p className='text-orange-300'>{defIndex + 1}.</p>
+                    <p className='text-blue-50 text-justify'>{definition}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <PagePlaceholder />
+          )
         )}
       </section>
     </div>
   )
 }
 
-export default WordMeaning;
+export default WordMeaning;  
---
+++ src/app/components/ui/PagePlaceholder.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+
+const PagePlaceholder = () => {
+  return (
+    <div className='flex w-full gap-8 justify-center pt-32'>
+      <div className='flex flex-col w-1/3'>
+        <h2 className='text-5xl font-bold'>Welcome to the Collegiate Resource Hub Dictionary Page.</h2>
+        <p className='text-2xl font-semibold text-justify mt-2'>This collegiate dictionary is a product of the Merriam-Webster Inc Open Source API.</p>
+        <p className='mt-2 text-sm'>All of the data rendered is provided by the API. Not all data will include an image, image context, etymology, or audio.</p>
+      </div>
+      <div className='flex flex-col'>
+        <Image
+          src='/Merriam-Webster_logo.svg.png'
+          alt='merriam-webster logo'
+          width={300}
+          height={300}
+          className='opacity-70'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default PagePlaceholder
